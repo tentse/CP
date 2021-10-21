6fabf618312574768cb20e0d186b9336162af159
TenTse <92934593+tentse@users.noreply.github.com>
2021-10-21 20:01:49 +0530

    Cpp code for checking prime number efficient way.

---
+++ SieveofEratosthenes.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<bool> primes(n+4,true);
+
+prime[0] = prime[1] = false;
+
+// Time complexity = O(nlog(logn))
+for (int i = 2; i*i <= n; i++){
+  if (prime[i]){
+    for (int j = i*i; j <=n; j+=i){
+      prime[i] = false;
+    }
+  }
+}
