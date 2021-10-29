64a79e265c5d58eac79b35b9474749c2727217de
TenTse <92934593+tentse@users.noreply.github.com>
2021-10-29 10:19:28 +0530

    New template

---
+++ template.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+
+#define For(i,a,b) for(int i = a; i<b; i++)
+#define FFor(i,a,b) for (int i = a; i<=b; i++)
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int,int> pi;
+
+bool comp(int &a,int &b){
+    if (a>=b){
+        return true;
+    }
+    return false;
+}
+
+bool comp(char &a, char &b){
+    if (a>=b){
+        return true;
+    }
+    rturn false;
+}
+
+//-----------------------------------------------------------------------------------------------
+
+void solve(){
+    
+}
+
+
+int main(){
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+
+    return 0;
+}
