5a89b3a31654248f39ee9ed7d359f111d422fd10
TenTse <92934593+tentse@users.noreply.github.com>
2021-11-02 11:24:39 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,61 +1,72 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 #define F first
 #define S second
 #define PB push_back
 #define EB emplace_back
 #define MP make_pair
 #define el "\n"
-
+ 
 #define For(i,a,b) for(int i = a; i<b; i++)
 #define Rfor(i,a,b) for (int i = a-1; i>=b; i--)
-
-
+#define loop(t) while(t--)
+ 
+ 
 typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
 typedef vector<char> vc;
 typedef vector<string> vs;
-typedef pair<int,int> pi;
+typedef pair<int,int> pairi;
 typedef map<int,int> mapi;
-typedef set<int> sti;
-typedef unordered_set<int> ust;
-
-
-
+typedef set<int> set;
+typedef multiset<int> mset;
+typedef unordered_set<int> uset;
+ 
 bool even(int &x){
     if (x&1 == 1){
         return false;
     }
     return true;
 }
-
+ 
+bool even(ll &x){
+    if (x&1 == 1){
+        return false;
+    }
+    return true;
+}
+ 
 bool comp(int &a,int &b){
     if (a>=b){
         return true;
     }
     return false;
 }
-
-
-//------------------------------------------------------------------------------------------------------------------------------------------------------------- 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+ 
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ 
+ 
 void solve(){
     
 
-
+ 
 }
-
-
+ 
+ 
+ 
 int main(){
     
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    
+    int t; cin >> t; while (t--)
 
     solve();
-
-
+    
     return 0;
 }
